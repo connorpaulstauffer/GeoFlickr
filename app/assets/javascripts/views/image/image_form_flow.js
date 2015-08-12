f7687be44252786dcd97ae36048c74abf555aea5
App Academy Student <student@appacademy.io>
2015-08-12 14:40:41 -0700

    add listeners to form flow

--- app/assets/javascripts/views/image/image_form_flow.js
+++ app/assets/javascripts/views/image/image_form_flow.js
@@ -1,9 +1,48 @@
 GeoFlickr.Views.ImageFormFlow = Backbone.CompositeView.extend({
   template: JST["images/image_form_flow"],
 
+  events: {
+    "click #right-control": "nextForm",
+    "click #left-control": "previousForm"
+  },
+
   initialize: function (options) {
     this._images = options.newImages,
-    this._modal = options.modal
+    this._modal = options.modal,
+    this._index = 0;
+    this.renderForms();
+  },
+
+  renderForms: function () {
+    var that = this;
+    this._images.each(function (image) {
+      var imageForm = new GeoFlickr.Views.ImageForm({ model: image });
+      that.addSubview("#image-form-container", imageForm);
+    });
+  },
+
+  activateCurrentForm: function () {
+    if (this._currentView) { this._currentView.deactivate() };
+    this._currentView = this.subviews("#image-form-container")[this._index];
+    this._currentView.activate();
+  },
+
+  nextForm: function () {
+    this.incrementIndex();
+    this.activateCurrentForm();
+  },
+
+  previousForm: function () {
+    this.decrementIndex();
+    this.activateCurrentForm();
+  },
+
+  incrementIndex: function () {
+    this._index = (this._index + 1) % this._images.length();
+  },
+
+  decrementIndex: function () {
+    this._index = (this._index === 0) ? this._images.length() : this._index - 1;
   },
 
   render: function () {
