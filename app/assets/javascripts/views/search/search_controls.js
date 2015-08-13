b8ff11f014ab0e63a3c90c21513b0132b6242090
App Academy Student <student@appacademy.io>
2015-08-13 09:37:47 -0700

    style search controls

--- app/assets/javascripts/views/search/search_controls.js
+++ app/assets/javascripts/views/search/search_controls.js
@@ -1,10 +1,12 @@
 GeoFlickr.Views.SearchControls = Backbone.View.extend({
   template: JST["search/search_controls"],
 
+  className: "col-sm-12",
+
   render: function () {
     var content = this.template();
     this.$el.html(content);
-    
+
     return this;
   }
 });
