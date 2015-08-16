6ab8a60a7946cfe5030b223214393f1504af9635
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-08-15 20:13:59 -0700

    render image on show page

--- app/assets/javascripts/views/image/image_show.js
+++ app/assets/javascripts/views/image/image_show.js
@@ -1,7 +1,11 @@
 GeoFlickr.Views.ImageShow = Backbone.CompositeView.extend({
   template: JST["images/image_show"],
 
-  className: "container-fluid",
+  className: "container-fluid image-show-container",
+
+  initialize: function () {
+    this.listenTo(this.model, "sync", this.render);
+  },
 
   render: function () {
     var content = this.template({ image: this.model })
