ee90791ee2b360f079209984e225645ba4dd12b8
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-08-16 08:23:51 -0700

    add image carousel subview to image show

--- app/assets/javascripts/views/image/image_show.js
+++ app/assets/javascripts/views/image/image_show.js
@@ -4,13 +4,21 @@ GeoFlickr.Views.ImageShow = Backbone.CompositeView.extend({
   className: "container-fluid image-show-container",
 
   initialize: function () {
-    this.listenTo(this.model, "sync", this.render);
+    // this.listenTo(this.model, "sync", this.render);
+    this.addImageCarousel();
+  },
+
+  addImageCarousel: function () {
+    var imageCarousel = new GeoFlickr.Views.ImageCarousel({
+      activeImage: this.model,
+      images: this.model.userImages();
+    })
   },
 
   render: function () {
-    var content = this.template({ image: this.model })
+    var content = this.template()
     this.$el.html(content);
-    // this.attachSubviews();
+    this.attachSubviews();
 
     return this;
   }
