c7d6a6bb1a571a1e467fe8dd485d04bfb0ff2ad4
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-08-16 18:05:20 -0700

    add user info view

--- app/assets/javascripts/views/user/user_info.js
+++ app/assets/javascripts/views/user/user_info.js
@@ -1,12 +1,19 @@
 GeoFlickr.Views.UserInfo = Backbone.View.extend({
   template: JST["users/user_info"],
 
+  tagName: "a",
+
+  attributes: function () {
+    return {
+      "href": "#/users/" + this.model.id
+    }
+  },
+
   initialize: function () {
     this.listenTo(this.model, "change", this.render);
   },
 
   render: function () {
-    debugger
     var content = this.template({
       user: this.model
     })
