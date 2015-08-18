81f09abe6e9035e501fe0372c317225fdc7092ec
App Academy Student <student@appacademy.io>
2015-08-18 07:39:15 -0700

    add styling to map

--- app/assets/javascripts/views/map/map_show.js
+++ app/assets/javascripts/views/map/map_show.js
@@ -21,12 +21,93 @@ GeoFlickr.Views.MapShow = Backbone.View.extend({
   },
 
   initializeMap: function () {
+    var styles = [
+                    {
+                        "featureType": "landscape.natural",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "color": "#e0efef"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "hue": "#1900ff"
+                            },
+                            {
+                                "color": "#c0e8e8"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "geometry",
+                        "stylers": [
+                            {
+                                "lightness": 100
+                            },
+                            {
+                                "visibility": "simplified"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "transit.line",
+                        "elementType": "geometry",
+                        "stylers": [
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "lightness": 700
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "water",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "color": "#7dcdcd"
+                            }
+                        ]
+                    }
+                ]
+    var styledMap = new google.maps.StyledMapType(styles, { name: "Styled Map" });
+
+
     var mapOptions = {
       zoomControlOptions: {
         style: google.maps.ZoomControlStyle.SMALL
+      },
+
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
       }
     };
+
     this._map = new google.maps.Map(this.el, mapOptions);
+    this._map.mapTypes.set("map_style", styledMap);
+    this._map.setMapTypeId("map_style");
     this._bounds = new google.maps.LatLngBounds();
     this._minZoom = 2;
     this.collection.each(this.addMarker.bind(this));
