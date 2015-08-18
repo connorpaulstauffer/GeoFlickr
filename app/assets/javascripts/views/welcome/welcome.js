4dd646040c0fef858686a58dda677c8f759abb27
App Academy Student <student@appacademy.io>
2015-08-18 13:27:13 -0700

    add animation to header

--- app/assets/javascripts/views/welcome/welcome.js
+++ app/assets/javascripts/views/welcome/welcome.js
@@ -33,11 +33,60 @@ GeoFlickr.Views.Welcome = Backbone.View.extend({
     this.$("#welcome-search").geocomplete();
   },
 
+  activateHeader: function () {
+    setTimeout(function () {
+      this.animateHeader("Beautiful", function () {
+        setTimeout(function () {
+          this.clearHeader(function () {
+            this.animateHeader("Awe Inspiring", function () {
+              setTimeout(function () {
+                this.clearHeader(function() {
+                  this.animateHeader("Your")
+                }.bind(this))
+              }.bind(this), 2000)
+            }.bind(this))
+          }.bind(this));
+        }.bind(this), 2000);
+      }.bind(this))
+    }.bind(this), 0);
+  },
+
+  animateHeader: function (text, callback) {
+    var idx = 0;
+    var $header = this.$("#header-adjective");
+
+    var headerInterval = setInterval(function () {
+      idx++;
+
+      if (idx >= text.length) {
+        clearInterval(headerInterval);
+        if (callback) { callback(); }
+      }
+
+      $header.html(text.substr(0, idx));
+    }.bind(this), 250)
+  },
+
+  clearHeader: function (callback) {
+    var $header = this.$("#header-adjective");
+
+    var headerInterval = setInterval(function () {
+      var headerVal = $header.text()
+      $header.text(headerVal.slice(0, -1));
+
+      if ($header.text().length === 0) {
+        clearInterval(headerInterval);
+        if (callback) { callback(); }
+      }
+    }.bind(this), 100)
+  },
+
   render: function () {
     var content = this.template();
     this.$el.html(content);
     this.setHeight();
     this.attachGeocomplete();
+    this.activateHeader();
 
     return this;
   }
