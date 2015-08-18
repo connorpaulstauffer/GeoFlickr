fdf4f7add70024fc732569e845de727c8f282db8
App Academy Student <student@appacademy.io>
2015-08-18 10:36:49 -0700

    add header to welcome page

--- app/assets/javascripts/views/welcome/welcome.js
+++ app/assets/javascripts/views/welcome/welcome.js
@@ -1,9 +1,16 @@
 GeoFlickr.Views.Welcome = Backbone.View.extend({
   template: JST["welcome/welcome"],
 
+  className: "welcome-container",
+
+  setHeight: function () {
+    this.$el.height($( window ).height())
+  },
+
   render: function () {
     var content = this.template();
     this.$el.html(content);
+    this.setHeight();
 
     return this;
   }
